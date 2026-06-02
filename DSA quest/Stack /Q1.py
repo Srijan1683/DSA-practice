4885e5f0a3ff092676e10a1bfa866f4c8a952ccf
Srijan1683 <pvtsrijan03@gmail.com>
2026-06-02 16:01:52 +0530

    Add solution for Building Array with Stack operations

---
+++ DSA quest/Stack /Q1.py
@@ -0,0 +1,36 @@
+"""Build an Array with Stack operations"""
+
+"""You are given an integer array target and an integer n.
+
+You have an empty stack with the two following operations:
+
+"Push": pushes an integer to the top of the stack.
+"Pop": removes the integer on the top of the stack.
+You also have a stream of the integers in the range [1, n].
+
+Use the two stack operations to make the numbers in the stack (from the bottom to the top) equal to target. You should follow the following rules:
+
+If the stream of the integers is not empty, pick the next integer from the stream and push it to the top of the stack.
+If the stack is not empty, pop the integer at the top of the stack.
+If, at any moment, the elements in the stack (from the bottom to the top) are equal to target, do not read new integers from the stream and do not do more operations on the stack.
+Return the stack operations needed to build target following the mentioned rules. If there are multiple valid answers, return any of them."""
+
+class Solution(object):
+    def buildArray(self, target, n):
+        """
+        :type target: List[int]
+        :type n: int
+        :rtype: List[str]
+        """
+        arr = []
+
+        for i in range (1,n+1):
+            if i in target:
+                arr.append("Push")
+            else:
+                if i <= target[-1]: 
+                    arr.append("Push")
+                    arr.append("Pop")
+                else:
+                    continue
+        return arr
