f718a0a49161ee43016d786de9b629cd57a4fe8e
Srijan1683 <pvtsrijan03@gmail.com>
2026-06-02 15:59:34 +0530

    Add solution for Disappeared numbers

---
+++ DSA quest/Array II/Q3.py
@@ -0,0 +1,16 @@
+"""Find all Numbers Disappeared in an Array"""
+
+"""Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums."""
+
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        for num in nums:
+            idx = abs(num) - 1
+            nums[idx] = -abs(nums[idx])
+
+        res = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                res.append(i + 1)
+
+        return res
