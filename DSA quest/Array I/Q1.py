c264d2ace3866235af4db9aed85d180739d9b72a
Srijan1683 <pvtsrijan03@gmail.com>
2026-06-02 15:52:27 +0530

    Add solution for Concatenation of Array

---
+++ DSA quest/Array I/Q1.py
@@ -0,0 +1,16 @@
+"""Concatenation of Array"""
+
+"""Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans."""
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = nums*2
+        return ans
