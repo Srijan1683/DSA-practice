46d4cf2ea75dc40e5ab34424495080d4122a840f
Srijan1683 <pvtsrijan03@gmail.com>
2026-08-03 23:37:00 +0530

    Add solution for Stone Game

---
+++ arrays/lc_877_stone_game.py
@@ -0,0 +1,11 @@
+"""877. Stone Game"""
+
+"""Problem: https://leetcode.com/problems/stone-game/description/"""
+
+class Solution(object):
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        return True
